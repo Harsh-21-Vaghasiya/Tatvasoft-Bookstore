48c5c99b91ab26e410a48a76a3cd7876fa04feda
Harsh <harshvaghasiya21@gmail.com>
2023-08-02 17:22:15 +0530

    UserEffect done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { Button, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const Form = () => {
 
     const [username, setusername] = useState("");
@@ -8,6 +8,12 @@ const Form = () => {
         username: username,
         password: password,
     });
+
+    useEffect(() => {
+        if (UserDetails.username.length > 4) {
+            console.log('use effect called with username');
+        }
+    }, [UserDetails.username]);
 
 
     const handleSubmit = () => {
@@ -18,15 +24,25 @@ const Form = () => {
     return (
         <>
             <div className="form-demo">
-                <Typography variant="h2">Login hear</Typography>
-                <TextField label="outlined"
+                <Typography variant="h2">Register hear</Typography>
+                <TextField label="Name"
                     variant="outlined"
                     value={UserDetails.username}
-                    onChange={(e) => setUserDetails({username:e.target.value})} />
-                <TextField label="outlined"
+                    onChange={(e) => setUserDetails((prev) => ({ ...prev, username: e.target.value }))} />
+                <TextField label="Email"
                     variant="outlined"
                     value={UserDetails.password}
-                    onChange={(e) => setUserDetails({password:e.target.value})}
+                    onChange={(e) => setUserDetails({ password: e.target.value })}
+                />
+                <TextField label="Age"
+                    variant="outlined"
+                    value={UserDetails.password}
+                    onChange={(e) => setUserDetails({ password: e.target.value })}
+                />
+                <TextField label="Password"
+                    variant="outlined"
+                    value={UserDetails.password}
+                    onChange={(e) => setUserDetails((prev) => ({ ...prev, password: e.target.value }))}
                 />
                 <Button variant="contained" type="submit" onClick={handleSubmit}>Submit</Button>
             </div>
